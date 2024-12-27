02daee33165706b288a8f2977517713745f2a6a9
mo-kasiri <mkgh7030@gmail.com>
2024-12-28 00:21:40 +0330

    update shaders

--- shaders/Holographic/Vertex.glsl
+++ shaders/Holographic/Vertex.glsl
@@ -2,7 +2,9 @@
 uniform float uTime;
 varying vec3 vPosition;
 varying vec3 vNormal;
-#include ../includes/random2D.glsl
+float random2D(vec2 value){
+    return fract(sin(uTime + dot(value.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
 
 void main()
 {
