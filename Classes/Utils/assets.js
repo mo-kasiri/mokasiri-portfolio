acb8654b280fbfa8ba5a2f51a4c50f0ecf7c1f0c
mo-kasiri <mkgh7030@gmail.com>
2024-12-29 21:09:42 +0330

    reduce filesize

--- Classes/Utils/assets.js
+++ Classes/Utils/assets.js
@@ -4,14 +4,9 @@ export default [
         type: "glbModel",
         path: "/models/bots.glb"
     },
-    {
+    /*{
         name:"screen",
         type: "videoTexture",
         path: "/textures/demo.gif"
-    },
-    {
-        name:"holographicBot",
-        type: "glbModel",
-        path: "/models/holographicBot.glb"
-    }
+    }*/
 ]
